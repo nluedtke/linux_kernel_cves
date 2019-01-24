fb3749d53baebf2ee7a857bf9b443f8bee3f5536
Nicholas Luedtke <nicholas.luedtke@uwalumni.com>
2019-01-23 20:14:06 -0500

    add comma to 404
    [ci skip]

--- ui/src/components/four.vue
+++ ui/src/components/four.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>FOUR OH FOUR</h1>
     <p>This CVE does not appear in our data. This could be because its too new (within hours), is embargoed, or we believe that it does not affect the upstream kernel.</p>
-    <p>If you believe this CVE does affect the Linux Kernel please fill out an <a href="https://github.com/nluedtke/linux_kernel_cves/issues/new?assignees=&labels=Data&template=cve-data-issue.md&title=%5BDATA%5D+CVE-XXXX-XXXXXX">issue</a>.</p>
+    <p>If you believe this CVE does affect the Linux Kernel, please fill out an <a href="https://github.com/nluedtke/linux_kernel_cves/issues/new?assignees=&labels=Data&template=cve-data-issue.md&title=%5BDATA%5D+CVE-XXXX-XXXXXX">issue</a>.</p>
   </div>
 </template>
 
